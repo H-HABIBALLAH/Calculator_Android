ca763db309d5fcad5837f10bad54f573bd62091c
HABIBALLAH <hamza1habiballah@gmail.com>
2021-03-24 02:55:12 +0100

    interface

--- src/main/java/com/example/calculatrice/MainActivity.java
+++ src/main/java/com/example/calculatrice/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.calculatrice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TableLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,8 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addStyle();
     }
 
     void collapseCollumns(){
+    }
+
+    void addStyle(){
+        for(int i=1;i<=7;i++){
+            View operators = findViewById(R.id.op1);
+            operators.setBackgroundColor(Color.GRAY);
+        }
     }
 }
